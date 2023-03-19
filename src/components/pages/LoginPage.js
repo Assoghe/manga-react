b0c671b4401e2a42558ebf72474d8827e19aeb3d
Assoghe <67897628+Assoghe@users.noreply.github.com>
2023-03-19 21:53:20 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
-  return (
-    <div>
-      Login Page
-    </div>
-  )
+
+    //state
+    const [inputValue, setInputValue] = useState("")
+
+    //comportements
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      alert(`Bonjour ${inputValue}`)
+    }
+
+    const handleChange = (event) => {
+      setInputValue(event.target.value)
+    }
+    
+    
+
+    //affichage (render)
+    return (
+        <div>
+            <h1> Bienvenue chez nous </h1>
+            <h2> Connectez-vous </h2>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input value={inputValue} onChange={handleChange} type="text" placeholder="entrez votre prenom..." required />
+                <button> Acceder à votre espace </button>
+            </form>
+        </div>
+    )
 }
