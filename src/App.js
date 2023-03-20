02156158039333412fa05a1234849c29e93ef7da
Assoghe <67897628+Assoghe@users.noreply.github.com>
2023-03-20 21:46:36 +0100

    ✨ F02: APP > Install routing system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/pages/login/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import MangaPage from './components/pages/manga/MangaPage';
+import ErrorPage from './components/pages/error/ErrorPage';
 
 function App() {
-  return <LoginPage/>
+
+  
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/manga" element={<MangaPage/>}/>
+      <Route path="*" element={<ErrorPage/>}/>
+
+    </Routes>
+  )
 }
 
 export default App;
